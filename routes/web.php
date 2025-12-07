f422faeec78c0a3875361e437fefe45a2726c371
abdelhakkoal7190 <abdelhak.koal@gmail.com>
2025-12-07 09:34:28 +0100

    Revert "8 upload"
    This reverts commit e7b54c59a5cb49dc7ae7714f4ff7878da5a381d4.

--- routes/web.php
+++ routes/web.php
@@ -8,8 +8,16 @@ use App\Http\Controllers\permissionControlles;
 use App\Http\Controllers\BobineController;
 use App\Http\Controllers\FibreColoriController;
 use App\Http\Controllers\TubLaicheController;
-
-
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 Auth::routes();
 Route::get('/', function () {
     return redirect()->route('usersList');
